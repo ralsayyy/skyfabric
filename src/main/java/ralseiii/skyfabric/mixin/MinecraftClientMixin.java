d767f8e49274a380ab1b9da701511fae4169a570
TotallyRobot <level77zockthd@gmail.com>
2021-05-06 15:10:16 +0200

    small changes

--- src/main/java/ralseiii/skyfabric/mixin/MinecraftClientMixin.java
+++ src/main/java/ralseiii/skyfabric/mixin/MinecraftClientMixin.java
@@ -2,12 +2,8 @@ package ralseiii.skyfabric.mixin;
 
 
 import ralseiii.skyfabric.Skyfabric;
-import ralseiii.skyfabric.utils.SbChecks;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
