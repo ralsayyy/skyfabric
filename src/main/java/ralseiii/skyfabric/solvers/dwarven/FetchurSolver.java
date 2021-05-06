3144001b1d1352fd56f6b7ddf7da07b2745c1b13
TotallyRobot <level77zockthd@gmail.com>
2021-05-06 15:12:46 +0200

    use translated strings in FetchurSolver

--- src/main/java/ralseiii/skyfabric/solvers/dwarven/FetchurSolver.java
+++ src/main/java/ralseiii/skyfabric/solvers/dwarven/FetchurSolver.java
@@ -1,20 +1,22 @@
 package ralseiii.skyfabric.solvers.dwarven;
 
+import net.minecraft.text.TranslatableText;
+
 public class FetchurSolver {
     public static String fetchurSolver(String message) {
-        if (message.contains("red and soft")) return "Red Wool";
-        if (message.contains("see through")) return "Yellow Stained Glass";
-        if (message.contains("circular and sometimes moves")) return "Compass";
+        if (message.contains("red and soft")) return new TranslatableText("tile.wool.red.name").toString();
+        if (message.contains("see through")) return new TranslatableText("tile.stained_glass.yellow.name").toString();
+        if (message.contains("circular and sometimes moves")) return new TranslatableText("item.compass.name").toString();
         if (message.contains("expensive minerals")) return "Mithril";
-        if (message.contains("its useful during celebrations")) return "Firework Rocket";
-        if (message.contains("gives energy")) return "Cheap coffee/Decent coffee";
-        if (message.contains("can be opened")) return "Wooden door";
-        if (message.contains("brown and fluffy")) return "Rabbit's foot";
+        if (message.contains("its useful during celebrations")) return new TranslatableText("item.fireworks.name").toString();
+        if (message.contains("gives energy")) return "Cheap coffee";
+        if (message.contains("can be opened")) return new TranslatableText("tile.doorWood.name").toString();
+        if (message.contains("brown and fluffy")) return new TranslatableText("item.rabbit_foot.name").toString();
         if (message.contains("explosive")) return "Superboom TNT";
-        if (message.contains("wearable")) return "Pumpkin";
-        if (message.contains("makes sparks")) return "Flint and Steel";
-        if (message.contains("red and white and you can mine it")) return "Nether Quartz Ore";
-        if (message.contains("round and green")) return "Ender Pearls";
+        if (message.contains("wearable")) return new TranslatableText("tile.pumpkin.name").toString();
+        if (message.contains("makes sparks")) return new TranslatableText("item.flint_and_steel.name").toString();
+        if (message.contains("red and white and you can mine it")) return new TranslatableText("tile.quartz_ore.name").toString();
+        if (message.contains("round and green")) return new TranslatableText("item.enderpearl.name").toString();
         return "";
     }
 }
