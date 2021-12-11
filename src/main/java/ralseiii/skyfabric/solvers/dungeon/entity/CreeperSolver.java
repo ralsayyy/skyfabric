78dbe7b32d202c6018e20f603ae4898f780e8aa0
TotallyRobot <level77zockthd@gmail.com>
2021-12-11 23:07:45 +0100

    continue working on creeper solver

--- src/main/java/ralseiii/skyfabric/solvers/dungeon/entity/CreeperSolver.java
+++ src/main/java/ralseiii/skyfabric/solvers/dungeon/entity/CreeperSolver.java
@@ -3,21 +3,17 @@ package ralseiii.skyfabric.solvers.dungeon.entity;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.mob.CreeperEntity;
+import ralseiii.skyfabric.utils.Position;
 import ralseiii.skyfabric.utils.RenderUtils;
+import ralseiii.skyfabric.utils.SbChecks;
 
 class LineCoords {
-    public float x;
-    public float y;
-    public float z;
-    public float endX;
-    public float endY;
-    public float endZ;
+    public Position startPos = new Position();
+    public Position endPos = new Position();
 }
 
 public class CreeperSolver {
-    public static double entityX;
-    public static double entityY;
-    public static double entityZ;
+    public static Position entityPos;
 
     public static boolean drawLines = false;
     public static LineCoords[] lineCoords = {
@@ -29,28 +25,24 @@ public class CreeperSolver {
 
     public static void register() {
         WorldRenderEvents.END.register(context -> {
-            if (drawLines) {
+            if (drawLines && SbChecks.isCatacombs) {
                 for (LineCoords l : lineCoords) {
-                    if (l.y == 0.0f)
-                        RenderUtils.renderSolidLine(l.x, l.y, l.z, l.endX, l.endY, l.endZ, context, 255, 0, 0, 0);
+                    if (l.startPos.y != 0.0f)
+                        RenderUtils.renderSolidLine((float) l.startPos.x, (float) l.startPos.y, (float) l.endPos.z, (float) l.endPos.x, (float) l.endPos.y, (float) l.endPos.z, context, 255, 0, 0, 0);
                 }
             }
         });
     }
     public static void solve() {
         MinecraftClient client = MinecraftClient.getInstance();
+        if (client == null || client.world == null || client.player == null) return;
         client.world.getEntitiesByClass(CreeperEntity.class, client.player.getBoundingBox().expand(20, 20, 20), entity -> {
             return !entity.isInvisible();
         }).forEach(entity -> {
-            entityX = entity.getX();
-            entityY = entity.getY();
-            entityZ = entity.getZ();
-            lineCoords[0].x = (float) entityX;
-            lineCoords[0].y = (float) entityY;
-            lineCoords[0].z = (float) entityZ;
-            lineCoords[0].endX = (float) entityX;
-            lineCoords[0].endY = (float) entityY + 10;
-            lineCoords[0].endZ = (float) entityZ;
+            entityPos.set(entity.getX(), entity.getY(), entity.getZ());
+            lineCoords[0].startPos.set(entityPos);
+            lineCoords[0].endPos.set(entityPos);
+            lineCoords[0].endPos.y += 10;
             drawLines = true;
         });
 
--- src/main/java/ralseiii/skyfabric/utils/Position.java
+++ src/main/java/ralseiii/skyfabric/utils/Position.java
@@ -9,4 +9,9 @@ public class Position {
         y = _y;
         z = _z;
     }
+    public void set(Position pos) {
+        x = pos.x;
+        y = pos.y;
+        z = pos.z;
+    }
 }
