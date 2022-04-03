63a1d0fc3f288da2bde2e14c6939268a649fc1a9
TotallyRobot <level77zockthd@gmail.com>
2022-04-03 14:08:32 +0200

    add copyright header

--- src/main/java/ralseiii/skyfabric/mixin/WorldAccessor.java
+++ src/main/java/ralseiii/skyfabric/mixin/WorldAccessor.java
@@ -1,5 +1,9 @@
 package ralseiii.skyfabric.mixin;
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.BlockEntityTickInvoker;
 import org.spongepowered.asm.mixin.Mixin;
